6c7298297b46c5d87acd7fc38431c7e1fcb388ea
Sho Yoshida <nya060@gmail.com>
2015-05-08 21:26:32 +0900

    not request_body json encoding when get

--- lib/rspec/request_describer.rb
+++ lib/rspec/request_describer.rb
@@ -27,7 +27,7 @@ module RSpec
         end
 
         let(:request_body) do
-          if headers["Content-Type"] == "application/json"
+          if headers["Content-Type"] == "application/json" && method != "get"
             params.to_json
           else
             params
